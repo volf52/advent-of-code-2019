233a2f21a3ebcaa52a7db96efdd453670b106636
Arslan <rslnkrmt2552@gmail.com>
2020-10-30 23:03:33 +0300

    Day 1 - Go version

---
+++ day1/day1.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func getInput(fname string) (numbers []int) {
+	fp, err := os.Open(fname)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = fp.Close(); err != nil {
+			log.Fatalf("error closing file: %s", err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(fp)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		tmp := scanner.Text()
+		num, err := strconv.Atoi(tmp)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		numbers = append(numbers, num)
+	}
+
+	return
+}
+
+func main() {
+	numbers := getInput("input")
+
+	totalOne := 0
+	totalTwo := 0
+	for _, num := range numbers {
+		num = num/3 - 2
+		totalOne += num
+
+		for num > 0 {
+			totalTwo += num
+			num = num/3 - 2
+		}
+	}
+
+	fmt.Printf("Part One -> %d\n", totalOne)
+	fmt.Printf("Part Two -> %d\n", totalTwo)
+}
