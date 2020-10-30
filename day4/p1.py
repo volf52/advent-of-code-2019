670de188a7dbbd7df1f4b19e4de41ca18c2a3abd
Arslan <rslnkrmt2552@gmail.com>
2020-10-30 17:39:52 +0300

    Day 4 - Part 2 - Pass

--- day4/p1.py
+++ day4/p1.py
@@ -15,19 +15,34 @@ def get_digits(n: int) -> List[int]:
     return digits
 
 
+def isNotPartOfGroup(idx: int, l: int, digits: List[int]) -> bool:
+    d = digits[idx]
+    if idx == 1:
+        return d != digits[idx] + 1
+    if idx == l:
+        return d != digits[idx - 2]
+
+    return d != digits[idx - 2] and d != digits[idx + 1]
+
+
 def meets_criteria(num: int) -> bool:
     digits = get_digits(num)
-    found_adjacent = False
+    adjacent_seen = {}
     for i in range(1, len(digits)):
         # should be non-decreasing
         if digits[i] < digits[i - 1]:
             return False
 
         if digits[i] == digits[i - 1]:
-            found_adjacent = True
+            k = (digits[i], digits[i])
+            adjacent_seen[k] = adjacent_seen.get(k, 0) + 1
 
-    return found_adjacent
+    return any(x == 1 for x in adjacent_seen.values())
 
+
+print(meets_criteria(112233))
+print(meets_criteria(123444))
+print(meets_criteria(11111122))
 
 total = 0
 for num in range(start, end + 1):
