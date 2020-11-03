23b23ded6f753dfbaa91f979312afe2af069d025
Arslan <rslnkrmt2552@gmail.com>
2020-11-03 21:02:02 +0300

    Day 5 - Python Version Complete

--- day5/day5.py
+++ day5/day5.py
@@ -11,7 +11,7 @@ class Interpreter:
     MAX_OPCODE = 8
 
     def __init__(self, memory: List[int], /, input_src: List[int] = None, is_part_two=False):
-        self._inp = (x for x in (input_src or [1]))
+        self._inp = (x for x in (input_src or []))
         self._out = []
         self._memory = memory.copy()
         self._ip = 0
@@ -35,23 +35,19 @@ class Interpreter:
 
         mode0 = int(instruction[2])
         mode1 = int(instruction[1])
-        mode2 = int(instruction[0])
+        # Checking the mode for arg2 (A) isn't really necessary
 
-        if mode2:
-            raise ValueError(f"No opcodes should have arg2 in immediate mode. {self._ip}")
-
-        arg0, arg1, arg2 = self._memory[self._ip + 1: self._ip + 4]
-
+        arg0 = self._memory[self._ip + 1]
 
         # Opcode is in range [MIN_OPCODE, MAX_OPCODE]
         if opcode < 3:  # b/w MIN_OPCODE and 2
+            arg1, arg2 = self._memory[self._ip + 2: self._ip + 4]
+
             arg0 = arg0 if mode0 else self._memory[arg0]
             arg1 = arg1 if mode1 else self._memory[arg1]
             res = MATH_OPS[opcode - 1](arg0, arg1)
-            if mode2:  # should not be true for these opcodes
-                print("Something looks wrong here")
-            else:
-                self._memory[arg2] = res
+
+            self._memory[arg2] = res
 
             new_ip = self._ip + 4
 
@@ -60,11 +56,13 @@ class Interpreter:
                 to_put = self.get_input()
                 self._memory[arg0] = to_put
             else:
-                self.send_output(self._memory[arg0])
+                self.send_output(arg0 if mode0 else self._memory[arg0])
 
             new_ip = self._ip + 2
 
         elif self._is_part_two:  # Opcode is 5-8. This branch runs only for part_two
+            arg1 = self._memory[self._ip + 2]
+
             arg0 = arg0 if mode0 else self._memory[arg0]
             arg1 = arg1 if mode1 else self._memory[arg1]
 
@@ -77,12 +75,16 @@ class Interpreter:
 
             elif opcode == 7:
                 new_ip += 1
+                arg2 = self._memory[self._ip + 3]
+
                 if arg0 < arg1:
                     self._memory[arg2] = 1
                 else:
                     self._memory[arg2] = 0
-            else:  # opcode is 8
+            elif opcode == 8:  # opcode is 8
                 new_ip += 1
+                arg2 = self._memory[self._ip + 3]
+
                 if arg0 == arg1:
                     self._memory[arg2] = 1
                 else:
@@ -111,13 +113,25 @@ class Interpreter:
 # Main Program
 if __name__ == "__main__":
     MEMORY = [int(x) for x in Path("input").read_text().split(",")]
-    # MEMORY = [1101, 100, -1, 5, 99, 33]
+    # MEMORY = [1101, 100, -1, 5, 99, 33] # Part one test
+
+    # Part Two tests
+    # MEMORY = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8] # input equal to 8 - pos mode
+    # MEMORY = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8] # input less than 8 - pos mode
+    # MEMORY = [3, 3, 1108, -1, 8, 3, 4, 3, 99]  # input equal to 8 - immediate mode
+    # MEMORY = [3, 3, 1107, -1, 8, 3, 4, 3, 99]  # input less than 8 - immediate mode
+    # MEMORY = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]  # is input non-zero - pos mode
+    # MEMORY = [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]  # is input non-zero - immediate mode
+
+    # MEMORY = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+    #           1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+    #           999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+    #           ]  # 999 in input below 8, 1000 if equal to 8, 1001 if above 8
 
     interpreter_one = Interpreter(MEMORY, [1])
-
     interpreter_one.process_instructions()
-
     print(interpreter_one.output[- 1])
-    # interpreter_two = Interpreter(MEMORY, [1], is_part_two=True)
-    # interpreter_two.process_instructions()
-    # print(interpreter_two.output[-1])
+
+    interpreter_two = Interpreter(MEMORY, [5], is_part_two=True)
+    interpreter_two.process_instructions()
+    print(interpreter_two.output[-1])
